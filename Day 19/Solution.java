3811a974cf2e28b60c8b4afaa1c93af86152cfb2
Maulikchhabra <46771434+Maulikchhabra@users.noreply.github.com>
2020-06-09 12:10:33 +0530

    Create Solution.java

---
+++ Day 19/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int result=1;
+
+        if(n==1){
+            return 1;
+        }
+
+        for(int i=2;i<=Math.sqrt(n);i++){
+
+            if(n%i==0){
+                if(i ==(n/i)){
+                    result=result+i;
+                }
+                else{
+                    result =result+i+(n/i);
+                }
+            }
+        }
+        result=result+n;
+        return result;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
